3f2d63d1df51f5234ed600edc96b3805d01c3c06
okisahandsome <okisahandsome@outlook.com>
2023-11-23 06:26:42 -0800

    download keyfile.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { FIREBASE_PROJECT_ID } = process.env;
-const CLOUD_STORAGE_BUCKET = `${FIREBASE_PROJECT_ID}.appspot.com`;
+const keyFilename = "./config/face-recognition-7f0db-aa3af157943e.json";
+const { projectId } = require(keyFilename);
+const bucketName = `${projectId}.appspot.com`;
 
 const gcs = require("@google-cloud/storage")({
   projectId,
@@ -8,49 +9,55 @@ const gcs = require("@google-cloud/storage")({
 
 const bucket = gcs.bucket(bucketName);
 
-// const filePath = `./gg.jpg`;
-// const uploadTo = `img/gg.jpg`;
-// const fileMime = mime.getType(filePath);
+const filePath = `./resources/google.jpg`;
+const uploadTo = `images/google.jpg`;
+const fileMime = mime.getType(filePath);
 
-// bucket.upload(filePath, {
-//   destination: uploadTo,
-//   public: true,
-//   metadata: { contentType: fileMime, cacheControl: "public, max-age=300" }
-// }, function (err, file) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(createPublicFileURL(uploadTo));
-// });
+bucket.upload(
+  filePath,
+  {
+    destination: uploadTo,
+    public: true,
+    metadata: { contentType: fileMime, cacheControl: "public, max-age=300" },
+  },
+  function (err, file) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(createPublicFileURL(uploadTo));
+  }
+);
 
-// function createPublicFileURL(storageName) {
-//   return `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(storageName)}`;
-// }
+function createPublicFileURL(storageName) {
+  return `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(
+    storageName
+  )}`;
+}
 
-const srcFilename = `img/gg.jpg`;
-const destFilename = `google.jpg`;
+// const srcFilename = `img/gg.jpg`;
+// const destFilename = `google.jpg`;
 
-const options = {
-  destination: destFilename,
-};
+// const options = {
+//   destination: destFilename,
+// };
 
-bucket
-  .file(srcFilename)
-  .download(options)
-  .then(() => {
-    console.log(
-      `gs://${bucketName}/${srcFilename} downloaded to ${destFilename}.`
-    );
-  })
-  .catch((err) => {
-    console.error("ERROR:", err);
-  });
+// bucket
+//   .file(srcFilename)
+//   .download(options)
+//   .then(() => {
+//     console.log(
+//       `gs://${bucketName}/${srcFilename} downloaded to ${destFilename}.`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("ERROR:", err);
+//   });
 
-/*
-//// you can check if a files exists or delete a file using the following
-const gcFile = bucket.file(uploadTo);
-gcFile.exists((err,exists)=>console.log(err||exists));
-gcFile.delete((err,res)=>console.log(err||res));
+// /*
+// //// you can check if a files exists or delete a file using the following
+// const gcFile = bucket.file(uploadTo);
+// gcFile.exists((err,exists)=>console.log(err||exists));
+// gcFile.delete((err,res)=>console.log(err||res));
 
-*/
+// */
